ff48c3568aa72ee96fb8b1fb952be18384a601b2
Mahedi Zaman Zaber <mahedizaber51@gmail.com>
2024-09-15 23:23:11 +0600

    Refactor Users model in users.py to improve code organization and add column definitions

--- app/Models/users.py
+++ app/Models/users.py
@@ -1,12 +1,14 @@
 from sqlalchemy import Column, String
 from database.db import BaseModel
 
+# Define the Users model class, inheriting from BaseModel
 class Users(BaseModel):
-    __tablename__ = 'users'
+    __tablename__ = 'users' # Define the table name
 
-    name = Column(String, index=True)
-    email = Column(String, unique=True, index=True)
-    password = Column(String)
+    name = Column(String, index=True) # Define the name column
+    email = Column(String, unique=True, index=True) # Define the email column
+    password = Column(String) # Define the password column
 
+    # Define a string representation for the Users model
     def __repr__(self):
         return f"<User(name={self.name}, email={self.email})>"
