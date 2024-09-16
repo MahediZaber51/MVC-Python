b0d86217d719b7c1ab196a666e160d01a6d849e8
Mahedi Zaman Zaber <mahedizaber51@gmail.com>
2024-09-17 01:32:58 +0600

    feat: Create basic controllers and implement initial functionality - Added greet_user function in the greetUser.py to handle user greetings. - Implemented basic structure for Flask and Discord bot controllers. - Added initial documentation for the greet_user function.

---
+++ app/Controllers/Discord/greetUser.py
@@ -0,0 +1,15 @@
+def greet_user(name):
+    """
+    Controller function to greet a user in Discord.
+
+    This function takes the name of a user and returns a greeting message. It can be used in a Discord bot
+    to send personalized greetings to users.
+
+    Args:
+        name (str): The name of the user.
+
+    Returns:
+        str: A greeting message.
+    """
+    # Do some processing here
+    return f"Hello, **{name}**!"
